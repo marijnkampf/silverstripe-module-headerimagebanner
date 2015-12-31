a7933e930df3c8f95aa6fbb63d37742631da06bd
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:44:56 +1300

    Converted to PSR-2

--- code/hibImage.php
+++ code/hibImage.php
@@ -1,23 +1,26 @@
 <?php
 
-class hibImage extends DataExtension {
-	static $belongs_many_many = array (
-		'HeaderImageBanners' => 'HeaderImageBanner'
-	);
-	
-	function hibCroppedImage() {
-		return $this->owner->croppedImage(HeaderImageBanner::$hibWidth, HeaderImageBanner::$hibHeight);
-	}
-	
-	function random() {
-		return rand();
-	}
+class hibImage extends DataExtension
+{
+    public static $belongs_many_many = array(
+        'HeaderImageBanners' => 'HeaderImageBanner'
+    );
+    
+    public function hibCroppedImage()
+    {
+        return $this->owner->croppedImage(HeaderImageBanner::$hibWidth, HeaderImageBanner::$hibHeight);
+    }
+    
+    public function random()
+    {
+        return rand();
+    }
 }
 
 
-class hibPageImage extends DataExtension {
-	static $belongs_many_many = array (
-		'HeaderImageBanners' => 'Page'
-	);
- 
+class hibPageImage extends DataExtension
+{
+    public static $belongs_many_many = array(
+        'HeaderImageBanners' => 'Page'
+    );
 }
